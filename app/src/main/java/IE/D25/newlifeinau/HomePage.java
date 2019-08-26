ce0acc5e9029e93a7d426fed614962a0abf35215
lwq57669292 <54259598+lwq57669292@users.noreply.github.com>
2019-08-26 20:26:01 +1000

    Add a method to based on device's IP address to get the Country and city information.

--- app/src/main/java/IE/D25/newlifeinau/HomePage.java
+++ app/src/main/java/IE/D25/newlifeinau/HomePage.java
@@ -1,21 +1,42 @@
 package IE.D25.newlifeinau;
 
+import android.app.ActivityManager;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.Inet4Address;
 import java.net.InetAddress;
+import java.net.MalformedURLException;
 import java.net.NetworkInterface;
 import java.net.SocketException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.Enumeration;
+import java.util.List;
 
 public class HomePage extends AppCompatActivity {
-    private Context context;
+
+    Context context = HomePage.this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +53,12 @@ public class HomePage extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+       ExternalIP externalIP= new ExternalIP();
+       externalIP.execute();
+
     }
+
 
     public static String getIP(Context context) {
 
@@ -51,6 +77,26 @@ public class HomePage extends AppCompatActivity {
         }
         return null;
     }
+
+
+
+    private class ExternalIP extends AsyncTask<Void, Void, String> {
+
+        protected String doInBackground(Void... urls) {
+            return IpAPI.search(GetIP.getNetIp(context));
+        }
+
+        protected void onPostExecute(String result) {
+
+            List<String> resultList = IpAPI.getInf(result);
+            if(!resultList.get(0).equals("Australia")) {
+                Toast.makeText(HomePage.this, "Only for Australia user", Toast.LENGTH_SHORT).show();
+                System.exit(0);
+            }
+        }
+    }
+
+
 
 
 }
